5bf347f0a6de6bf63599fc302bb67acccdeea1c5
Fsociety-Mrn <lisboamillen30@gmail.com>
2023-03-08 14:12:18 +0800

    updated

---
+++ FaceDetection/Jolo_Detection.py
@@ -0,0 +1,74 @@
+import cv2
+import requests
+import time
+
+from io import BytesIO
+
+#  setup camera
+camera = cv2.VideoCapture(0)
+camera.set(4,1080)
+
+# Load face detector
+face_detector = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+def facialDetection(face_recognition_url ='http://192.168.100.36:1030/face-recognition'):
+    R , G , B = 0,255,0
+    Text = ""
+    # Initialize the timer and the start time
+    timer = 0
+    start_time = time.time()
+    
+    while True:
+        
+        # Capture a frame from the camera
+        ret, frame = camera.read()
+        
+        if not ret:
+            break
+
+        frame = cv2.flip(frame,1)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            
+        # Detect faces in the frame
+        faces = face_detector.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=20, minSize=(100, 100), flags=cv2.CASCADE_SCALE_IMAGE)
+
+        # checking detecting face should be 1
+        if len(faces) == 1:
+            # Get the coordinates of the face
+            (x, y, w, h) = faces[0]
+
+            # Draw a rectangle around the face
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            
+            cv2.putText(frame,Text,(x,y+h+30),cv2.FONT_HERSHEY_COMPLEX,1,(R,G,B),1)
+                            
+            # Check if 2 seconds have elapsed since the last send
+            if timer >= 2:
+                        
+                # Encode the frame as a JPEG image
+                img_encoded  = cv2.imencode('.jpg', frame)[1].tobytes()
+                
+                # Send the JPEG image to the face recognition API
+                response = requests.post(
+                    face_recognition_url, 
+                    files={
+                            'file': ('image.jpg', BytesIO(img_encoded), 'image/jpeg')
+                        })
+ 
+                if response.status_code == 200:
+                    R , G , B = 0,255,0
+                    Text = response.text
+                else:
+                    R , G , B = 255,0,0
+                    Text = response.text
+                    
+                # Reset the timer and the start time
+                timer = 0
+                start_time = time.time()
+            else:
+                # Increment the timer by the elapsed time since the last send
+                timer += time.time() - start_time
+                start_time = time.time()
+                    
+            _, frame_encoded  = cv2.imencode('.jpg', frame)
+            yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame_encoded.tobytes() + b'\r\n')
--- app.py
+++ app.py
@@ -1,48 +1,13 @@
 import cv2
-
-from flask import Flask, jsonify, request,render_template,Response
-
 import requests
+import time
+
+from flask import Flask, render_template,Response
 from io import BytesIO
+from FaceDetection.Jolo_Detection import facialDetection
+
 
 app = Flask(__name__)
-
-#  setup camera
-camera = cv2.VideoCapture(0)
-camera.set(4,1080)
-
-# face detection
-face_detector = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-
-
-def generate_frames(url=''):
-    
-    while True:
-        success, frame = camera.read()
-        if not success:
-            break
-        else:
-            frame = cv2.flip(frame,1)
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            
-            # Detect faces in the frame
-            faces = face_detector.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=20, minSize=(100, 100), flags=cv2.CASCADE_SCALE_IMAGE)
-
-            if len(faces) == 1:
-                for (x, y, w, h) in faces:
-                    face = frame[y:y+h, x:x+w]
-                    # draw reactnagle on face detected
-                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2) 
-                    
-                    img_bytes = cv2.imencode('.jpg', frame)[1].tobytes()
-                    response = requests.post(url, files={'file': ('image.jpg', BytesIO(img_bytes), 'image/jpeg')})
-                    print(response.text)
-                    
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
@@ -50,7 +15,7 @@ def index():
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(facialDetection(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
