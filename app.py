2e052c5e4781a05888d9c622f260b5fc950d8486
Fsociety-Mrn <lisboamillen30@gmail.com>
2023-03-13 16:23:24 +0800

    upadted

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template,Response
+from flask import Flask, render_template,Response,request
 from FaceDetection.Jolo_Detection import facialDetection,facialRegister
 
 import cv2
 app = Flask(__name__)
 
-
+# this block of code is for entry code
 
 @app.route('/')
 def index():
@@ -24,6 +24,7 @@ def video_feed():
     return Response(facialDetection(camera=camera, face_detector=faceetector), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
+# this block of code is for registration
 @app.route('/register')
 def register():
     return render_template('register.html')
@@ -38,6 +39,16 @@ def registerFacial():
     faceetector = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     
     return Response(facialRegister(camera=camera, face_detector=faceetector), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# subumit form
+
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    first_name = request.form.get('firstname')
+    last_name = request.form.get('lastname')
+    print(first_name + last_name)
+    # do something with the form data
+    return render_template('face.html')
 
 
 if __name__ == '__main__':
