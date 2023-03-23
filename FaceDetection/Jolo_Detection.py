f46d06311a3e5e6837f061738352b0c4b2dcd841
ajhits <104299235+ajhits@users.noreply.github.com>
2023-03-24 01:31:13 +0800

    Add files via upload

--- FaceDetection/Jolo_Detection.py
+++ FaceDetection/Jolo_Detection.py
@@ -1,19 +1,12 @@
 import cv2
-import requests
 import time
 import webbrowser
 
-from flask import redirect,url_for
+from FaceDetection.Jolo_Recognition import JoloRecognition as Jolo
 
+form_path = 'http://127.0.0.1:3030/'
 # ============================== facial register ============================== #
-def facialRegister(local=None,url=None,camera=None, face_detector=None):
-    
-    # load facial register API endpoints
-    face_recognition_url = url + '/facial-register'
-    
-    timer = 0
-    start_time = time.time()
-    capture = 1
+def facialRegister(camera=None, face_detector=None):
     
     while True:
         
@@ -34,33 +27,6 @@ def facialRegister(local=None,url=None,camera=None, face_detector=None):
         # NOTE: if more than 1 it wont detected and the timer will restart
         if len(faces) == 1:
             
-            # The frame capture is sent to API once every 30 seconds.
-            if timer >= 0.5:
-                
-                # Encode the frame as a JPEG image
-                img_encoded  = cv2.imencode('.png', frame, [cv2.IMWRITE_JPEG_QUALITY, 100])[1]
-                
-                # Send the JPEG image to the face recognition API
-                response = requests.post(
-                    face_recognition_url, 
-                    files={
-                            'file': (str(capture)+".png", img_encoded.tobytes(), 'image/png')
-                        },
-                    timeout=10
-                    )
-
-                # if the respond is successfull trained
-                if response.text == "Successfully trained":
-                    # it will open new tab for main page
-                    return webbrowser.open("http://" + str(local) + "/")
-               
-                timer = 0
-                start_time = time.time()
-                capture +=1
-            else:
-                timer += time.time() - start_time
-                start_time = time.time()
-            
             # Get the coordinates of the face
             (x, y, w, h) = faces[0]
             
@@ -75,12 +41,14 @@ def facialRegister(local=None,url=None,camera=None, face_detector=None):
 
 
 # ============================== facial recognition ============================== #
-def facialDetection(url='http://192.168.100.36:1030', camera=None, face_detector=None):
+def facialDetection(camera=None, face_detector=None):
     
-    # load facial recognition API endpoints
-    face_recognition_url = url + '/face-recognition'
     R , G , B = 0,255,0
     Text = ""
+    
+    # Login Attempt
+    success = 1
+    fail = 1                                    
     
     # Initialize the timer and the start time
     timer = 0
@@ -96,7 +64,7 @@ def facialDetection(url='http://192.168.100.36:1030', camera=None, face_detector
 
         frame = cv2.flip(frame,1)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            
+        
         # Detect faces in the frame
         faces = face_detector.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=20, minSize=(100, 100), flags=cv2.CASCADE_SCALE_IMAGE)
    
@@ -104,24 +72,45 @@ def facialDetection(url='http://192.168.100.36:1030', camera=None, face_detector
         if len(faces) == 1:
                             
             # Check if 2 seconds have elapsed since the last send
-            if timer >= 0.8:
-                        
-                # Encode the frame as a JPEG image
-                img_encoded  = cv2.imencode('.png', frame, [cv2.IMWRITE_JPEG_QUALITY, 100])[1]
+            if timer >= 1:
                 
-                # Send the JPEG image to the face recognition API
-                response = requests.post(
-                    face_recognition_url, 
-                    files={
-                        'file': ('image.png',img_encoded.tobytes(), 'image/png')
-                    },
-                    timeout=10)
+                # facial comparison 
+                response = Jolo().Face_Compare(face=frame)
+                
+                try:
                 
                 # check if there is matches and if status code is 200 
-                B, G, R = (255, 0, 0) if response.text == 'No match detected' else (0, 255, 0) if response.status_code == 200 else (0, 0, 255)
-                Text = response.text
-
+                    B, G, R = (255, 0, 0) if response[0] == 'No match detected' else (0, 255, 0)
+                    Text = response[0]
                     
+         
+                    # check if face is recognize
+                    if "No match detected" != response[0] and success < 3:
+                        success += 1
+                    else: 
+                        # check if not 
+                        fail += 1
+                    
+                        # check if fail
+                        if fail == 5:
+                            Text = "Access Denied"
+                            fail = 1
+                             
+
+                            # route to failure pages
+                    
+                        # check if success      
+                        if success == 3:
+                            Text = "Access granted"
+                            success = 1
+                            fail=1
+                            webbrowser.open(form_path)
+                            # route to succes pages
+                    
+                        
+                        
+                except:
+                    pass
                 # Reset the timer and the start time
                 timer = 0
                 start_time = time.time()
@@ -134,9 +123,32 @@ def facialDetection(url='http://192.168.100.36:1030', camera=None, face_detector
             (x, y, w, h) = faces[0]
             
             # Draw a rectangle around the face and pt text
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (R,G,B), 2)
+            #cv2.rectangle(frame, (x, y), (x+w, y+h), (R,G,B), 2)
             cv2.putText(frame,Text,(x,y+h+30),cv2.FONT_HERSHEY_COMPLEX,1,(R,G,B),1)
-                    
+            
+            
+
+
+            # Center coordinates
+            center_coordinates = x + w // 2, y + h // 2
+  
+            # Radius of circle
+            radius = w // 2
+   
+            # Blue color in BGR
+            color = (R, G, B)
+   
+#            Line thickness of 2 px
+            thickness = 3
+            cv2.circle(frame, center_coordinates, radius, color, thickness)
+            
+            
+        else:
+            
+            # If more than 1 faces 
+            B, G, R = (255, 0, 0)
+            Text = "More than 1 face is detected"
+                  
         _, frame_encoded  = cv2.imencode('.png', frame)
         yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + frame_encoded.tobytes() + b'\r\n')
